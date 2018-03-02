dd7f2f2abdcc8381a9ae11de1dfd5d65dfe8e95d
Alex Kibler <alexkibler@me.com>
2018-03-02 11:45:38 -0500

    add db to gitignore

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ const { app, BrowserWindow } = require('electron')
 const huejay = require('huejay')
 // const path = require('path')
 // const url = require('url')
+const Datastore = require('nedb')
+
+const db = {}
+
+db.users = new Datastore('./db/users.db')
+db.commands = new Datastore('./db/commands.db')
+
+db.users.loadDatabase()
+db.commands.loadDatabase()
 
 let win
 
